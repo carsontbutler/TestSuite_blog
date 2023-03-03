154ed6ac4ef459dfca4e41111ea8afe4b36c670e
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-03-02 19:37:45 -0600

    added blank registration test

--- tests/test_register.py
+++ tests/test_register.py
@@ -12,6 +12,14 @@ class TestRegister(BaseTest):
         home_pg.click_register_link()
         assert register_pg._driver.title == register_pg.page_title
 
+    def test_register_with_all_fields_blank(self, go_home):
+        register_pg = self.pages['register_page']
+        home_pg = self.pages['home_page']
+
+        home_pg.click_register_link()
+        register_pg.click(register_pg.REGISTER_BTN)
+        assert register_pg.get_element(register_pg.USERNAME_FIELD).get_attribute("validationMessage") == register_pg.BLANK_INPUT_ERROR
+
     def test_register_with_blank_username(self, go_home):
         register_pg = self.pages['register_page']
         home_pg = self.pages['home_page']
@@ -47,15 +55,15 @@ class TestRegister(BaseTest):
         assert register_pg.get_element(register_pg.PASSWORD2_FIELD).get_attribute("validationMessage") == register_pg.BLANK_INPUT_ERROR
 
     def test_register_with_blank_email(self, go_home):
-            register_pg = self.pages['register_page']
-            home_pg = self.pages['home_page']
+        register_pg = self.pages['register_page']
+        home_pg = self.pages['home_page']
 
-            home_pg.click_register_link()
-            register_pg.send_keys(register_pg.USERNAME_FIELD, data['username'])
-            register_pg.send_keys(register_pg.PASSWORD1_FIELD, data['password'])
-            register_pg.send_keys(register_pg.PASSWORD2_FIELD, data['password'])
-            register_pg.click(register_pg.REGISTER_BTN)
-            assert register_pg.get_element(register_pg.EMAIL_FIELD).get_attribute("validationMessage") == register_pg.BLANK_INPUT_ERROR
+        home_pg.click_register_link()
+        register_pg.send_keys(register_pg.USERNAME_FIELD, data['username'])
+        register_pg.send_keys(register_pg.PASSWORD1_FIELD, data['password'])
+        register_pg.send_keys(register_pg.PASSWORD2_FIELD, data['password'])
+        register_pg.click(register_pg.REGISTER_BTN)
+        assert register_pg.get_element(register_pg.EMAIL_FIELD).get_attribute("validationMessage") == register_pg.BLANK_INPUT_ERROR
 
     def test_register_with_invalid_email_1(self, go_home):
         register_pg = self.pages['register_page']
