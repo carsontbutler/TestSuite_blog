cb28f5c2bb73da1c1ee5579a0cebb254b5a2212b
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-03-02 23:13:22 -0600

    updated data

--- test_data.py
+++ test_data.py
@@ -1,7 +1,9 @@
 data = {
-    'username': 'TestUser123',
-    'email': 'testing@test.com',
-    "password": "TopSecret000",
-    "email_invalid_1": "testing@testing",
-    "email_invalid_2": "testing.com"
+    "register": {'username': 'TestUser123',
+                 'email': 'testing@test.com',
+                 "password": "TopSecret111",
+                 "email_invalid_1": "testing@testing",
+                 "email_invalid_2": "testing.com"},
+    "login": {"username": "TestUser001",
+              "password": "TopSecret000"}
 }
