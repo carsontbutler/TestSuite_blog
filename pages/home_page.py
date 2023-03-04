e81305b265d6a531af9b53a44cabb5f704bb7164
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-03-04 07:52:16 -0600

    replaced .click_login/register_page buttons with standard .click()

--- pages/home_page.py
+++ pages/home_page.py
@@ -7,14 +7,8 @@ class HomePage(BasePage):
     #Top Navigation
     LOGIN_LINK = (By.XPATH, '//*[@id="navbarToggle"]/div[2]/a[1]')
     REGISTER_LINK = (By.XPATH, '//*[@id="navbarToggle"]/div[2]/a[2]')
+    ABOUT_LINK = (By.XPATH, '//*[@id="navbarToggle"]/div[1]/a[2]')
     LOGOUT_LINK = (By.XPATH, '//*[@id="navbarToggle"]/div[2]/a[3]')
 
     #Misc
     ALERT = (By.CLASS_NAME, 'alert')
-
-
-    def click_register_link(self):
-        self.click(self.REGISTER_LINK)
-
-    def click_login_link(self):
-        self.click(self.LOGIN_LINK)
