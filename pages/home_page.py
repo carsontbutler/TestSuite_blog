f176d7c9cfaf11932a5ffa261ee78f1c5f447348
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-03-04 16:46:20 -0600

    added test_view_post

--- pages/home_page.py
+++ pages/home_page.py
@@ -11,5 +11,8 @@ class HomePage(BasePage):
     NEW_POST_LINK = (By.XPATH, '//*[@id="navbarToggle"]/div[2]/a[1]')
     LOGOUT_LINK = (By.XPATH, '//*[@id="navbarToggle"]/div[2]/a[3]')
 
+    #Posts
+    POST_LINK = (By.CLASS_NAME, 'article-title')
+
     #Misc
     ALERT = (By.CLASS_NAME, 'alert')
---
+++ tests/test_view_post.py
@@ -0,0 +1,20 @@
+from TestSuite.tests.test_base import BaseTest
+from TestSuite.test_data import data
+import pytest
+
+
+@pytest.mark.order(5)
+class TestViewPost(BaseTest):
+    # as of right now this finds the most recently created post
+    # so it has to run after the new_post tests
+
+    def test_view_post(self, go_home):
+        home_pg = self.pages['home_page']
+        view_post_pg = self.pages['view_post_page']
+
+        home_pg.click(home_pg.POST_LINK)
+
+        assert view_post_pg.get_element(
+            view_post_pg.POST_TITLE).text == data['blog']['title']
+        assert view_post_pg.get_element(
+            view_post_pg.POST_CONTENT).text == data['blog']['content']
