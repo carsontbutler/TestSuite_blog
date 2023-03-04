4f99dbaf11364057ecbe88b97c82d5d7ebb915e3
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-03-04 07:51:32 -0600

    about page test additions

--- pages/about_page.py
+++ pages/about_page.py
@@ -1,10 +1,15 @@
 from TestSuite.pages.base_page import BasePage
+from selenium.webdriver.common.by import By
 
 class AboutPage(BasePage):
 
     #Properties
+    
 
     #Locators
+    PAGE_HEADER = (By.XPATH, '/html/body/main/div/div[1]/h1')
 
     #Expected outputs
+    PAGE_HEADER_TEXT = 'About this website'
+    
     pass
--- tests/test_about.py
+++ tests/test_about.py
@@ -3,4 +3,11 @@ from TestSuite.tests.test_base import BaseTest
 from TestSuite.test_data import data
 
 class TestAbout(BaseTest):
-    pass
+    
+    def test_go_to_about_page(self, go_home):
+        home_pg = self.pages['home_page']
+        about_pg = self.pages['about_page']
+
+        home_pg.click(home_pg.ABOUT_LINK)
+        
+        assert about_pg.get_element(about_pg.PAGE_HEADER).text == about_pg.PAGE_HEADER_TEXT
