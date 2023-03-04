9c4eae7c7f5f6cc7f7e545081a9c7c06c026709f
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-03-04 07:53:32 -0600

    about test additions

--- tests/test_about.py
+++ tests/test_about.py
@@ -2,12 +2,14 @@ import pytest
 from TestSuite.tests.test_base import BaseTest
 from TestSuite.test_data import data
 
+
 class TestAbout(BaseTest):
-    
+
     def test_go_to_about_page(self, go_home):
         home_pg = self.pages['home_page']
         about_pg = self.pages['about_page']
 
         home_pg.click(home_pg.ABOUT_LINK)
-        
-        assert about_pg.get_element(about_pg.PAGE_HEADER).text == about_pg.PAGE_HEADER_TEXT
+
+        assert about_pg.get_element(
+            about_pg.PAGE_HEADER).text == about_pg.PAGE_HEADER_TEXT
