018864be9ea6e51ad34233f5dfee830cc83409d2
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-03-06 12:55:49 -0600

    added profile pic

--- test_data.py
+++ test_data.py
@@ -1,4 +1,6 @@
+import os
 base_url = 'http://localhost:8000'
+base_dir = os.path.dirname(os.path.abspath(__file__))
 data = {
     "profile": {'username': 'TestUser123',
                 'username_changed': 'TestUser456',
@@ -6,7 +8,9 @@ data = {
                 'email_changed': 'changed@test.net',
                 "password": "TopSecret111",
                 "email_invalid_1": "testing@testing",
-                "email_invalid_2": "testing.com"},
+                "email_invalid_2": "testing.com",
+                "new_profile_image_name": "bob.jpg"
+                },
     "blog": {"title": "My Blog Post 1",
              "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt lorem nisl, non iaculis quam efficitur vitae. Aliquam erat volutpat. Aliquam ut vulputate metus, eget suscipit elit."}
 }
