1d361c0fa09c72375a4e9d4cdaeea655cb92a9e0
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2023-03-06 17:23:42 -0600

    added profile tests

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -1,13 +1,14 @@
 from TestSuite.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
+
 class ProfilePage(BasePage):
-    
-    #Properties
+
+    # Properties
     slug = 'profile'
     page_title = 'Django Blog'
 
-    #Locators
+    # Locators
     PROFILE_NAME = (By.CLASS_NAME, 'account-heading')
     USERNAME_FIELD = (By.ID, 'id_username')
     EMAIL_FIELD = (By.ID, 'id_email')
@@ -15,6 +16,8 @@ class ProfilePage(BasePage):
     IMAGE_FIELD = (By.ID, 'id_image')
     UPDATE_BTN = (By.XPATH, '/html/body/main/div/div[1]/div/form/div/button')
     ALERT = (By.XPATH, '/html/body/main/div/div[1]/div[1]')
+    USERNAME_ERROR_LOCATOR = (By.ID, 'error_1_id_username')
 
-    #Expected outputs
+    # Expected outputs
     UPDATE_SUCCESS_TEXT = 'Your account has been updated!'
+    INVALID_USERNAME_ERROR_1 = 'Enter a valid username. This value may contain only letters, numbers, and @/./+/-/_ characters.'
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,7 +1,7 @@
 import pytest
 from TestSuite.tests.test_base import BaseTest
 from TestSuite.test_data import data, base_url, base_dir
-import time
+from TestSuite.helpers import *
 
 
 class TestProfile(BaseTest):
@@ -28,8 +28,8 @@ class TestProfile(BaseTest):
             'value') == data['profile']['email']
         assert profile_pg.get_element(
             profile_pg.IMAGE_TITLE).text == 'default.jpg'
-        
-    #USERNAME TESTS
+
+    # USERNAME TESTS
     def test_change_username(self, go_home):
         home_pg = self.pages['home_page']
         profile_pg = self.pages['profile_page']
@@ -43,16 +43,35 @@ class TestProfile(BaseTest):
             profile_pg.ALERT).text == profile_pg.UPDATE_SUCCESS_TEXT
         assert profile_pg.get_element(profile_pg.USERNAME_FIELD).get_attribute(
             'value') == data['profile']['username_changed']
-        
+
     def test_change_username_invalid(self, go_home):
-        #enter username with forbidden characters and make sure it is rejected
-        pass
+        home_pg = self.pages['home_page']
+        profile_pg = self.pages['profile_page']
+
+        home_pg.click(home_pg.PROFILE_LINK)
+        profile_pg.send_keys(profile_pg.USERNAME_FIELD,
+                             generate_invalid_username())
+        profile_pg.click(profile_pg.UPDATE_BTN)
+
+        assert profile_pg.get_element(
+            profile_pg.USERNAME_ERROR_LOCATOR).text == profile_pg.INVALID_USERNAME_ERROR_1
+
+    def test_change_long_username(self, go_home):
+        home_pg = self.pages['home_page']
+        profile_pg = self.pages['profile_page']
+
+        home_pg.click(home_pg.PROFILE_LINK)
+        profile_pg.send_keys(profile_pg.USERNAME_FIELD,
+                             generate_long_username())
+
+        assert len(profile_pg.get_element(
+            profile_pg.USERNAME_FIELD).get_attribute('value')) == 150
 
     def test_change_username_preexisting(self, go_home):
-        #enter username that already exists and make sure it is rejected
+        # enter username that already exists and make sure it is rejected
         pass
 
-    #EMAIL TESTS
+    # EMAIL TESTS
     def test_change_email(self, go_home):
         home_pg = self.pages['home_page']
         profile_pg = self.pages['profile_page']
@@ -66,16 +85,16 @@ class TestProfile(BaseTest):
             profile_pg.ALERT).text == profile_pg.UPDATE_SUCCESS_TEXT
         assert profile_pg.get_element(profile_pg.EMAIL_FIELD).get_attribute(
             'value') == data['profile']['email_changed']
-        
+
     def test_change_email_invalid_1(self, go_home):
-        #change email to data["profile"]["email_invalid_1"]
+        # change email to data["profile"]["email_invalid_1"]
         pass
 
     def test_change_email_invalid_2(self, go_home):
-        #change email to data["profile"]["email_invalid_2"]
+        # change email to data["profile"]["email_invalid_2"]
         pass
-    
-    #PROFILE PIC TESTS
+
+    # PROFILE PIC TESTS
     def test_change_profile_pic(self, go_home):
         home_pg = self.pages['home_page']
         profile_pg = self.pages['profile_page']
